c508eec102c2db646c514525e1134805022a7d3d
liuhaitao <13519155665@.163.com>
2022-05-25 09:11:39 +0800

    management

--- src/pages/Home/Contents/NetWork/NightShift/NewTurnClass/index.jsx
+++ src/pages/Home/Contents/NetWork/NightShift/NewTurnClass/index.jsx
@@ -13,35 +13,42 @@ class NewTurnClass extends Component {
     state = {
         user: [],
         turnEffect: [],
-        mail: ''
+        userMail: '',
     }
 
     onFinish = (values) => {
         console.log('Received values of form:', values);
     };
 
-    handlemail = ()=>{
-        
+    handlemail = () => {
+        return (a, b) => {
+            const { user } = this.state
+            user.forEach((userItem) => {
+                if (userItem.name === b.children) {
+                    this.setState({ userMail: userItem.email })
+                }
+            })
+        }
     }
 
     async componentDidMount() {
         const newUser = []
 
         const result = await axios.post('/api/username')
-        
+
         result.data.forEach(element => {
-            newUser.push({...element})
+            newUser.push({ ...element })
         });
 
-        this.setState({user:newUser})
+        this.setState({ user: newUser })
 
     }
 
     render() {
-        const {user} = this.state
+        const { user } = this.state
         return (
             <QueueAnim className="queue-simple">
-                <Form name="dynamic_form_nest_item" onFinish={this.onFinish} autoComplete="off" style={{padding:15, marginTop:30,background:'#fff'}}>
+                <Form name="dynamic_form_nest_item" onFinish={this.onFinish} autoComplete="off" style={{ padding: 15, marginTop: 30, background: '#fff' }}>
                     <Title style={{ paddingTop: 20, marginLeft: 20 }} level={3}>转班交接</Title>
                     <Divider />
                     <Form.List name="users" >
@@ -56,58 +63,68 @@ class NewTurnClass extends Component {
                                         }}
                                         align="baseline"
                                     >
-                                        <Col>
-                                            <Form.Item
-                                                label='交接PIC'
-                                                {...restField}
-                                            >
-                                                {localStorage.getItem('username')}
-                                            </Form.Item>
-                                        </Col>
-                                        <Col>
-                                            <Form.Item
-                                                label='交接事件'
-                                                {...restField}
-                                                name={[name, 'first']}
-                                                rules={[
-                                                    {
-                                                        required: true,
-                                                        message: 'Missing Effect',
-                                                    },
-                                                ]}
-                                            >
-                                                <Input placeholder="please input" />
-                                            </Form.Item>
-                                        </Col>
-                                        <Col >
-                                            <Form.Item
-                                                style={{width:150}}
-                                                label='PIC'
-                                                {...restField}
-                                                name={[name, 'last']}
-                                                rules={[
-                                                    {
-                                                        required: true,
-                                                        message: 'Missing last name',
-                                                    },
-                                                ]}
-                                            >
-                                                <Select
-                                                onBlur={this.handlemail}
+                                        <Row style={{display:'inlineBlock'}}>
+                                            <Col style={{marginRight:20}}>
+                                                <Form.Item
+                                                    label='交接PIC'
+                                                    {...restField}
                                                 >
-                                                    {user.map((item)=>{
-                                                        return (
-                                                            <Option key={item._id}>{item.name}</Option>
-                                                        )
-                                                    })}
-                                                </Select>
-                                            </Form.Item>
-                                        </Col>
-                                        <Col>
+                                                    {localStorage.getItem('username')}
+                                                </Form.Item>
+                                            </Col>
+                                            <Col style={{marginRight:20}}>
+                                                <Form.Item
+                                                    label='交接事件'
+                                                    {...restField}
+                                                    name={[name, 'effect']}
+                                                    rules={[
+                                                        {
+                                                            required: true,
+                                                            message: 'Missing Effect',
+                                                        },
+                                                    ]}
+                                                >
+                                                    <Input placeholder="please input" />
+                                                </Form.Item>
+                                            </Col>
+                                            <Col style={{marginRight:100}}>
+                                                <Form.Item
+                                                    style={{ width: 150 }}
+                                                    label='PIC'
+                                                    {...restField}
+                                                >
+                                                    <Select
+                                                        mode="multiple"
+                                                        allowClear
+                                                        style={{
+                                                            width: 200,
+                                                        }}
+                                                        placeholder="Please select"
+                                                        // defaultValue={['a10', 'c12']}
+                                                        onChange={this.handlemail()}
+                                                    >
+                                                        {user.map((item) => {
+                                                            return (
+                                                                <Option key={item._id}>{item.name}</Option>
+                                                            )
+                                                        })}
+                                                    </Select>
+                                                </Form.Item>
+                                            </Col>
+                                            {/* <Col>
                                             <Form.Item>
                                                 {this.state.mail}
                                             </Form.Item>
-                                        </Col>
+                                        </Col> */}
+                                            <Col style={{marginRight:20}}>
+                                                <Form.Item
+                                                    label='备注'
+                                                >
+                                                    <Input placeholder="please input" />
+                                                </Form.Item>
+                                            </Col>
+                                        </Row>
+
                                         <MinusCircleOutlined onClick={() => remove(name)} />
                                     </Space>
                                 ))}
